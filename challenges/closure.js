32a3e50dec69a7753547e0f6cb8476096696d0f8
Tom Sherman <tompsherman@gmail.com>
2020-08-21 12:14:00 -0400

    closure

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,16 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// its like russian nesting dolls; the innermost functions and reach upwards because they are part of the higher order functions. another way to look at it is the { } act like gates the nestedFunction is inside the gates of myFunction, and therefor has access to any variables available. however, if nestedFunction had a unique variable, myFunction would not have access to it, bc it would be outside the {gates}
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(number){
+  let counter = 0;
+  for(let i=0; i < number; i++){
+    counter = (i+1) + counter
+    console.log(counter)
+  }
+}
+console.log(summation(4));
